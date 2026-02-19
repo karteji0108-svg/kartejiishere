b149608256d60bb120a326aade248ed40bde7489
karteji0108-svg <karteji0108@gmail.com>
2026-02-19 12:07:11 +0700

    Update Vite config for base path and output settings

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: '/',
   plugins: [react()],
   esbuild: {
     drop: mode === 'production' ? ['console', 'debugger'] : [],
   },
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
-        output: {
-            manualChunks(id) {
-                if (id.includes('node_modules')) {
-                    return 'vendor';
-                }
-            }
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
         }
+      }
     }
   }
 }))
